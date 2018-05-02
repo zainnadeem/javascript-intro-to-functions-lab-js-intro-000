64701082186335f0850dcac30e72b587c69c25e8
Zain Nadeem <zn.nadeem@gmail.com>
2018-05-02 21:16:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.lowercase()
+}
+
+function logShot(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if string == string.lowercase() {
+    return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if string == string.uppercase() {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if string == "I love you, Grandma." {
+    return "I love you, too."
+  }
+}
