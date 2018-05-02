2ae6d12ea0cd4fe426ebf311b5c04c15b958b311
Zain Nadeem <zn.nadeem@gmail.com>
2018-05-02 21:20:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toUpperCase() {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
 }
 
 function sayHiToGrandma(string) {
-  if string === "I love you, Grandma." {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
